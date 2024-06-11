be06def9c13ec13cfec50155dd79a2a4c9b5dacb
Kelechi Ibekwe <kibekwe96@gmail.com>
2024-06-11 07:42:11 -0600

    Added services to application

--- apigw/src/main/java/com/kelechitriescoding/apigw/ApiGWApplication.java
+++ apigw/src/main/java/com/kelechitriescoding/apigw/ApiGWApplication.java
@@ -1,16 +1,13 @@
-package com.kelechitriescoding.fraud;
+package com.kelechitriescoding.apigw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-import javax.swing.*;
-
 @SpringBootApplication
 @EnableEurekaClient
-public class  FraudApplication {
-    public static void main (String[] args){
-
-        SpringApplication.run(FraudApplication.class, args);
+public class ApiGWApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ApiGWApplication.class,args);
     }
 }
